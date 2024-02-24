33d6d355a0ba683da34bbe9e43fe3492b7e94968
Tayfun <tayfun.ceylan@outlook.de>
2024-02-24 19:17:20 +0100

    - added components - added login and register page

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -3,20 +3,30 @@
   import * as Card from '$lib/components/ui/card';
   import { Input } from '$lib/components/ui/input';
   import { Label } from '$lib/components/ui/label';
+  import { page } from '$app/stores';
+
+  let username = $page.url.searchParams.get('username');
+  let token = $page.url.searchParams.get('token');
 </script>
 
 <!-- center card -->
-<Card.Root class="w-[350px] mx-auto mt-20">
+<Card.Root class="mx-auto mt-20 w-[350px]">
   <Card.Header>
-    <Card.Title>Login</Card.Title>
-    <!-- <Card.Description>Deploy your new project in one-click.</Card.Description> -->
+    <Card.Title>Register</Card.Title>
+    <Card.Description
+      >Gib ein Passwort ein, um dich zu registrieren. Sonst musst du nichts weiter erledigen.</Card.Description
+    >
   </Card.Header>
   <Card.Content>
     <form>
       <div class="grid w-full items-center gap-4">
         <div class="flex flex-col space-y-1.5">
+          <Label for="token">Token</Label>
+          <Input id="token" placeholder="Enter token" disabled bind:value={token} />
+        </div>
+        <div class="flex flex-col space-y-1.5">
           <Label for="username">Username</Label>
-          <Input id="username" placeholder="Enter username" />
+          <Input id="username" placeholder="Enter token" disabled bind:value={username} />
         </div>
         <div class="flex flex-col space-y-1.5">
           <Label for="password">Password</Label>
